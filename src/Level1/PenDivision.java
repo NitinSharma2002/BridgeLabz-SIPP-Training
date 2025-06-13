622d9bc2ea05743a8433abd1a2a3d8e10eed1232
NitinSharma2002 <nitinsudhashamra321@gmail.com>
2025-06-13 08:47:39 +0530

    [Nitin] level1 completed

---
+++ src/Level1/PenDivision.java
@@ -0,0 +1,11 @@
+package Level1;
+
+public class PenDivision {
+    public static void main(String[] args) {
+        int pens = 14;
+        int Students = 3;
+        int remaining_pens = pens % Students;
+        int pens_per_Student = pens / Students;
+        System.out.println("The Pens per Student is: " + pens_per_Student + " and the remaining Pens not distributed is: " + remaining_pens);
+    }
+}
