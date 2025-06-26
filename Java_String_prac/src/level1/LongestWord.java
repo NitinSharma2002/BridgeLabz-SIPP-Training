dc293531f4e8393384986fdafcd29ee45268896a
NitinSharma2002 <nitinsudhashamra321@gmail.com>
2025-06-26 12:25:59 +0530

    [Nitin] Assignment Done

---
+++ Java_String_prac/src/level1/LongestWord.java
@@ -0,0 +1,15 @@
+package level1;
+
+public class LongestWord {
+    public static void main(String[] args) {
+        String sentence = "Java is a language";
+        String[] words = sentence.split(" ");
+        String longest = "";
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+        System.out.println("Longest word: " + longest);
+    }
+}
---
+++ Java_String_prac/src/level1/SubstringOccurrences.java
@@ -0,0 +1,16 @@
+package level1;
+
+public class SubstringOccurrences {
+    public static void main(String[] args) {
+        String str = "ababababtxyz";
+        String sub = "ab";
+        int count = 0;
+
+        for (int i = 0; i <= str.length() - sub.length(); i++) {
+            if (str.substring(i, i + sub.length()).equals(sub)) {
+                count++;
+            }
+        }
+        System.out.println("Occurrences of '" + sub + "': " + count);
+    }
+}
