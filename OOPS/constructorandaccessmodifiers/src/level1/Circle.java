73ff2c4494879ec4fc68d043977003518c4e6f90
NitinSharma2002 <nitinsudhashamra321@gmail.com>
2025-07-01 10:45:30 +0530

    [Nitin] constructor and Modifiers assignment done

---
+++ OOPS/constructorandaccessmodifiers/src/level1/Circle.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class Circle {
+    double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public Circle(){
+        this.radius = 2.0;
+    }
+}
