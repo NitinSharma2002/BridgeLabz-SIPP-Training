58181cbebc2aa6dcda6414e9212b7fe8b395a632
NitinSharma2002 <nitinsudhashamra321@gmail.com>
2025-07-07 07:45:00 +0530

    [Nitin] Inheritance Assignment done

---
+++ OOPS/inheritance/singlelevel/src/SmartHomeDevice/Main.java
@@ -0,0 +1,8 @@
+package SmartHomeDevice;
+
+public class Main {
+    public static void main(String[] args) {
+        Thermostat myThermostat = new Thermostat("THERMO123", true, 22.5);
+        myThermostat.displayStatus();
+    }
+}
