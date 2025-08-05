73145951c31d25148966a88f2cb6092b1a731ce6
NitinSharma2002 <nitinsudhashamra321@gmail.com>
2025-08-05 08:53:56 +0530

    [Nitin] generics Assignment done

---
+++ src/generics/Collections/MAP/GroupEmployeesByDepartment/Employee.java
@@ -0,0 +1,24 @@
+package generics.Collections.MAP.GroupEmployeesByDepartment;
+
+public class Employee {
+    private String name;
+    private String department;
+
+    public Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
